a1dc5d2b749b7fc3249eafc95e648d5b78a9e94e
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2023-06-28 15:42:59 +0530

    feat(app): add constants

---
+++ app/lib/src/store/constants/assets.dart
@@ -0,0 +1,13 @@
+import 'package:tyrion/src/utils/theme/theme.dart';
+
+abstract class AppAssets {
+  static String get logo =>
+      'assets/images/logo/${AppTheme.isDark ? 'dark' : 'light'}/logo.svg';
+  static String get logoTransparent =>
+      'assets/images/logo/${AppTheme.isDark ? 'dark' : 'light'}/logo-transparent.svg';
+
+  static String get errorRobot => 'assets/images/error/error-robot.svg';
+
+  static String get launchVideo =>
+      'assets/videos/launch_animations/${AppTheme.isDark ? 'dark' : 'light'}.mp4';
+}
---
+++ app/lib/src/store/constants/strings.dart
@@ -0,0 +1,49 @@
+abstract class AppStrings {
+  // TODO: convert into localization class
+  // TODO: add helper function to replace **STRING** with a value
+
+  // Main App Strings
+  static const String appName = 'Project Tyrion';
+  static const String appDescription = 'Your AI-Enabled Personal Assistant';
+  static const String appVersion = '0.0.1';
+  static const String appHomeTitle = 'Home';
+
+  // Main Error Strings
+  static const String error =
+      'Oops! I think I broke something.\nMaybe catch me a little later?';
+
+  // Main Loading Strings
+
+  // Game Dashboard Strings
+  static const String gameDashboardTitle = 'Your Games';
+
+  // Common Words
+  static const String commonWordUpdate = 'Update';
+  static const String commonWordUpdates = 'Updates';
+  static const String commonWordLatest = 'Latest';
+  static const String commonWordRecommended = 'Recommended';
+  static const String commonWordOverview = 'Overview';
+  static const String commonWordFollowing = 'Following';
+  static const String commonWordFavourite = 'Favourite';
+  static const String commonWordFavourites = 'Favourites';
+  static const String commonWordSearch = 'Search';
+  static const String commonWordConfirm = 'Confirm';
+  static const String commonWordClose = 'Close';
+  static const String commonWordCancel = 'Cancel';
+  static const String commonWordContinue = 'Continue';
+  static const String commonWordRetry = 'Retry';
+  static const String commonWordClear = 'Clear';
+  static const String commonWordYes = 'Yes';
+  static const String commonWordNo = 'No';
+  static const String commonWordAccept = 'Accept';
+  static const String commonWordDecline = 'Decline';
+  static const String commonWordOk = 'Ok';
+  static const String commonWordDone = 'Done';
+  static const String commonWordNext = 'Next';
+  static const String commonWordBack = 'Back';
+  static const String commonWordFinish = 'Finish';
+  static const String commonWordSkip = 'Skip';
+  static const String commonWordSave = 'Save';
+  static const String commonWordDelete = 'Delete';
+  static const String commonWordRemove = 'Remove';
+}
