25007d1866c75491ec3c363e1f715d412ce941a0
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2023-08-06 01:12:37 +0530

    feat(functions): add env vars and update tool configs

--- functions/nodejs/src/env.ts
+++ functions/nodejs/src/env.ts
@@ -27,3 +27,29 @@ export const NODE_ENV = defineString('NODE_ENV', {
     },
   },
 });
+
+export const GAMES_MAIN_SOURCE_LINK = defineString('GAMES_MAIN_SOURCE_LINK', {
+  description: 'The complete web address from which games are sourced.',
+  input: {
+    text: {
+      example: 'itch.io',
+      validationRegex:
+        /^(?:([a-zA-Z0-9-]+?)\.)?([a-zA-Z0-9-]+?)\.([a-z]{2,}|[a-z]{2,5}\.[a-z]{2})$/,
+      validationErrorMessage: 'Pease enter a valid address',
+    },
+  },
+});
+
+export const GAMES_MAIN_SOURCE_USERNAME = defineString(
+  'GAMES_MAIN_SOURCE_USERNAME',
+  {
+    description: 'The username to authenticate the main games source',
+  },
+);
+
+export const GAMES_MAIN_SOURCE_PASSWORD = defineString(
+  'GAMES_MAIN_SOURCE_PASSWORD',
+  {
+    description: 'The password to authenticate the main games source',
+  },
+);
